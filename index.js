a82dc99f17f193f800dc503dada8fde6612255ff
elazar125 <elazar125@gmail.com>
2017-11-28 23:58:27 -0600

    Rewrite code from the old angular service to node

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const express = require('express');
+const https = require('https');
 const app = express();
-const graph = require('./graph.js');
-const routes = require('./routes.js');
 
-const port = 8000;
-
-routes(app, graph);
-
-app.listen(port, () => {
-    console.log('Running on port ' + port);
+app.get('/events', (req, res) => {
+    getAuthorized(
+        'events',
+        'name,cover,description,start_time,end_time,is_canceled',
+        (response) => res.send(response
+            .filter((event) => new Date(event.start_time) > new Date())
+            .sort((a, b) => new Date(a.start_time) - new Date(b.start_time))));
 });
+
+app.get('/photos', (req, res) => {
+    getAuthorized(
+        'photos/uploaded',
+        'images',
+        (response) => res.send(response
+            .map((i) => i.images[0])));
+});
+
+app.listen(8000);
+
+function getAuthorized(edge, fields, cb) {
+    var url = 'https://graph.facebook.com/oauth/access_token'
+        + '?client_id=' + 'app id'
+        + '&client_secret=' + 'secret'
+        + '&grant_type=client_credentials'
+
+    get(url, (res) => {
+        var url = 'https://graph.facebook.com/v2.10/cavalierfencingmb/' + edge
+            + '?access_token=' + res.access_token
+            + '&fields=' + fields
+
+        get(url, (res) => cb(res.data));
+    });
+}
+
+function get(url, cb) {
+    https.get(url, (res) => {
+        const body = [];
+        res.on('data', (chunk) => body.push(chunk));
+        res.on('end', () => cb(JSON.parse(body.join(''))));
+    }).on('error', (e) => console.error(e));
+}
